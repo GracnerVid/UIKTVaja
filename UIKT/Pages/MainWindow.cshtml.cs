3dcbf8828a62ae55905b8fa12b7ebaf707ae41e0
Vid Gračner <vid.gracner@student.um.si>
2023-01-14 17:38:31 +0100

    dodelana shramba snosa podatkov

--- UIKT/Pages/MainWindow.cshtml.cs
+++ UIKT/Pages/MainWindow.cshtml.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UIKT.Resources;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace UIKT.Pages
 {
@@ -7,6 +10,34 @@ namespace UIKT.Pages
     {
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var ime = Request.Form["fname"];
+            var priimek = Request.Form["fpass"];
+            var emso = Request.Form["femso"];
+            var davcnast = Request.Form["fdst"];
+            var iban = Request.Form["fiban"];
+            var naziv = Request.Form["fnaziv"];
+            var opis = Request.Form["fopis"];
+
+            if (ime == "" || priimek == "" || emso == "" || davcnast == "" ||
+                iban == "" || naziv == "" || opis == "")
+            {
+                //return RedirectToPage("/MainWindow");
+                return Page();
+            }
+            else
+            {
+                Vloga vloga = new Vloga(ime, priimek, emso, davcnast, iban, naziv, opis);
+                string json = JsonConvert.SerializeObject(vloga);
+                string fileName = "vloga" + DateTime.Today.ToString("dd-MM-yyyy") + ".json";
+                string folderPath = Path.GetFullPath(@"Vloge");
+                string filePath = Path.Combine(folderPath, fileName);
+                System.IO.File.WriteAllText(filePath, json);
+                return RedirectToPage("/Index");
+            }
         }
     }
 }
--- UIKT/Pages/login.cshtml.cs
+++ UIKT/Pages/login.cshtml.cs
@@ -8,5 +8,30 @@ namespace UIKT.Pages
         public void OnGet()
         {
         }
+
+        //protected void loginBtn_Click(object sender, EventArgs e)
+        //{
+        //    if (true)
+        //    {
+        //        return RedirectToPage("");
+        //    }
+        //}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var username = Request.Form["fname"];
+            var password = Request.Form["fpass"];
+
+            if (username == "admin" && password == "password")
+            {
+                //HttpContext.Session.SetString("IsLoggedIn", "true");
+                return RedirectToPage("/MainWindow");
+            }
+            else
+            {
+                //HttpContext.Session.SetString("IsLoggedIn", "false");
+                return Page();
+            }
+        }
     }
 }
